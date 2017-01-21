160dec323a31fbf670751e0699ff271c2861ede0
Erik Sundberg <erik@sundberg.io>
2017-01-20 22:33:20 -0800

    Example tube man

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float upForce = 100f;
+	public float horzForce = 20f;
+
+	Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Vertical") > 0){
+			Debug.Log("up");
+			rigidBody.AddForce(Vector3.up*upForce);
+		}
+		rigidBody.AddForce(this.transform.right* Input.GetAxis("Horizontal") * horzForce);
+	}
+}
