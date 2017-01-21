b2d0fc48aabd5d5fa36c39ba86cba21c12ac8731
Erik Sundberg <erik@sundberg.io>
2017-01-21 11:06:10 -0800

    Added crowd animations

---
+++ Assets/Cheer/AudienceScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudienceScript : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Awake (){
+		animator = GetComponent<Animator>();
+		if(Random.Range(0f, 1f) > 0.5){
+			animator.SetBool("cheer", true);
+			animator.CrossFade("cheer",0);
+		}
+	}
+
+	void Start () {
+		AnimatorStateInfo state = animator.GetCurrentAnimatorStateInfo(0);
+		animator.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
