3bc254835a8c17e6753b135b8394b9563d04912b
Erik Sundberg <erik@sundberg.io>
2017-01-21 14:18:30 -0800

    Tube man and audience testing

---
+++ Assets/Audience/CrowdController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowdController : MonoBehaviour {
+
+	private string[] names = {"idle","applause","applause2","celebration","celebration2","celebration3"};
+
+	// Use this for initialization
+	void Start () {
+		Animation[] AudienceMembers = gameObject.GetComponentsInChildren<Animation>();
+		foreach(Animation anim in AudienceMembers){
+			string thisAnimation = names[Random.Range(0,5)];
+
+			anim.wrapMode = WrapMode.Loop;
+			anim.CrossFade(thisAnimation);
+			anim[thisAnimation].time = Random.Range(0f,3f);
+		}
+	}
+}
