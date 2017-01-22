1820ae9d8054a2992f3683a153a6d71e52b573b1
Erik Sundberg <erik@sundberg.io>
2017-01-21 22:26:39 -0800

    Fixed tubeman distortion

---
+++ Assets/HeadLook.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadLook : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(Camera.main.transform.position - this.transform.position), 1);
+	}
+}
--- Assets/Tubeman Test/PlayerScript.cs
+++ Assets/Tubeman Test/PlayerScript.cs
@@ -22,7 +22,7 @@ public class PlayerScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		if(Input.GetButton("Jump") && !isInflating){
 			StartCoroutine(Inflate());
 		}
