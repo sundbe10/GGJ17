b4f73cadd7e4e10a3c631695f0d81d1332d8edd9
Erik Sundberg <erik@sundberg.io>
2017-01-22 16:09:38 -0800

    Score Bar

---
+++ Assets/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+
+	public static int score = 500;
+
+	GameObject[] players;
+
+	// Use this for initialization
+	void Start () {
+		players = GameObject.FindGameObjectsWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Assets/ScoreBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBar : MonoBehaviour {
+
+	public GameObject player;
+
+	ScoreManager scoreManager;
+	RectTransform scoreBar;
+
+	// Use this for initialization
+	void Start () {
+		scoreManager = player.GetComponentInChildren<ScoreManager>();
+		scoreBar = transform.Find("Score Bar").GetComponent<RectTransform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(200*scoreManager.score/GameManager.score);
+		scoreBar.sizeDelta = new Vector2(200*scoreManager.score/GameManager.score,10);
+	}
+}
